2afe09c9bc5df813cdbf7ddbe94a757f93b84463
inhaq <p126013@nu.edu.pk>
2019-04-18 15:52:22 +0500

    path update for production

--- src/js/useIcon.js
+++ src/js/useIcon.js
@@ -1,20 +1,20 @@
 function getIcon(data) {
   if (data.id >= 200 && data.id < 300){
-    return './src/img/storm.png';
+    return './../src/img/storm.png';
   } else if (data.id >= 300 && data.id < 400){
-    return './src/img/mist.png';
+    return './../src/img/mist.png';
   } else if (data.id >= 400 && data.id < 500){
-    return './src/img/clouds.png';
+    return './../src/img/clouds.png';
   } else if (data.id >= 500 && data.id < 600){
-    return './src/img/rain.png';
+    return './../src/img/rain.png';
   } else if (data.id >= 600 && data.id < 700){
-    return './src/img/snow.png';
+    return './../src/img/snow.png';
   } else if (data.id >= 700 && data.id < 800){
-    return './src/img/mist.png';
+    return './../src/img/mist.png';
   } else if (data.id === 800){
-    return './src/img/sunny.png';
+    return './../src/img/sunny.png';
   } else if (data.id >= 800){
-    return './src/img/clouds.png';
+    return './../src/img/clouds.png';
   }
 }
 
